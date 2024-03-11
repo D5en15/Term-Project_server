68d49021db5a1ed9420f4721c485028c538647c7
D5en15 <D5en15.14on@gmail.com>
2024-03-11 14:07:15 +0700

    step15

--- models/server.js
+++ models/server.js
@@ -22,7 +22,7 @@ class Server {
 
     // Pick up React index.html file
     this.app.use(
-      express.static(path.join(__dirname, "../../client/build"))
+      express.static(path.join(__dirname, "../../server/build"))
     );
   }
 
@@ -34,7 +34,7 @@ class Server {
     // Catch all requests that don't match any route
     this.app.get("*", (req, res) => {
       res.sendFile(
-        path.join(__dirname, "../../client/build/index.html")
+        path.join(__dirname, "../../server/build/index.html")
       );
     });
   }
